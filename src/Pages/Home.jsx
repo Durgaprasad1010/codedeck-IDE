d26fe24597c4493fe5d562a11447b5ed95404dd5
Durgaprasad <durgaprasadptr10@gmail.com>
2023-01-23 17:37:26 +0530

    landing page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,11 +4,11 @@ import LeftPaneScreen from '../Components/Home/LeftPaneScreen';
 function Home() {
     return (
         <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2'>
-            <div className='md:w-full sm:w-full w-4/12 border-2'>
+            <div className='md:w-full sm:w-full '>
                 <LeftPaneScreen />
             </div>
 
-            <div className='md:w-full sm:w-full w-8/12 border-2'>
+            <div className='md:w-full sm:w-full '>
                 <RightPaneScreen />
             </div>
         </div>
